7fc21cd96f8b3314ff2e96088bb4a0fd15a283b1
Andreas Müller <andreas.w.mueller@outlook.com>
2017-05-23 21:14:09 +0200

    Defined the amounts as part of a class
    Problem: Because objects are constants, loop is no longer possible

--- tkp.rb
+++ tkp.rb
@@ -1,3 +1,4 @@
+=begin
 wkk = 36 # Wie viele Kreuzer sind ein Taler? (default 36)
 wkp = 6 # Wie viele Pfennige sind ein Kreuzer? (default 6)
 
@@ -11,86 +12,71 @@ puts "Dieser Umrechnungskurs kann im Script über die Variablen <wkk> und <wkp> 
 puts ""
 puts "Wichtig: Bitte verwenden Sie nur 0 sowie ganze Zahlen zur Eingabe. Ergebnisse von Divisionen werden immer auf den letzten Pfennig abgerundet."
 puts "Viel Erfolg!"
+=end
+class Amount
+  $wkk = 36
+  $wkp = 6
+  def initialize(talerIn, kreuzerIn, pfennigeIn)
+    @talerIn = talerIn
+    @kreuzerIn = kreuzerIn
+    @pfennigeIn = pfennigeIn
+    @pfenTot = @talerIn*$wkk*$wkp + @kreuzerIn*$wkp + @pfennigeIn
+    @talerOut = @pfenTot / ($wkk*$wkp) .floor
+    @kreuzerOut = (@pfenTot - (@talerOut*$wkk*$wkp)) / $wkp .floor
+    @pfennigeOut = (@pfenTot - (@talerOut*$wkk*$wkp) - (@kreuzerOut*$wkp))
+  end
+  def totErg
+    puts "Das Ergebnis lautet #{@talerOut} Taler, #{@kreuzerOut} Kreuzer, #{@pfennigeOut} Pfennige!"
+  end
+  def totPret
+    return @pfenTot
+  end
+end
 
-loop do
-  puts "Bitte geben Sie den ersten Betrag ein!"
-  puts "Wie viele Taler?"
-  talerIn1 = gets.to_i
-  puts "Wie viele Kreuzer?"
-  kreuzerIn1 = gets.to_i
-  puts "Wie viele Pfennige?"
-  pfennigeIn1 = gets.to_i
-  pfennigeTot1 = (talerIn1*wkk*wkp) + (kreuzerIn1*wkp) + pfennigeIn1
-  loop do
+#loop do
+  puts "Bitte geben Sie den ersten Betrag ein! (Taler, Kreuzer, Pfennige)"
+  Am1 = Amount.new(gets.to_i, gets.to_i, gets.to_i)
+  #loop do
     puts "Bitte wählen Sie die Rechenoperation, die Sie durchführen möchten (+), (-), (/), (*), (=)"
     op = gets.chomp
     case op
     when "+"
-      puts "Bitte geben Sie den zweiten Betrag ein, den Sie addieren möchten!"
-      puts "Wie viele Taler?"
-      talerIn2 = gets.to_i
-      puts "Wie viele Kreuzer?"
-      kreuzerIn2 = gets.to_i
-      puts "Wie viele Pfennige?"
-      pfennigeIn2 = gets.to_i
-      pfennigeTot2 = (talerIn2*wkk*wkp) + (kreuzerIn2*wkp) + pfennigeIn2
-      ergebnis = pfennigeTot1.to_i + pfennigeTot2.to_i
-      talerOut = ergebnis / (wkk*wkp) .floor
-      kreuzerOut = (ergebnis - (talerOut*wkk*wkp)) / wkp .floor
-      pfennigeOut = (ergebnis - (talerOut*wkk*wkp) - (kreuzerOut*wkp))
-      puts "#{talerIn1} Taler, #{kreuzerIn1} Kreuzer und #{pfennigeIn1} Pfennige #{op} #{talerIn2} Taler, #{kreuzerIn2} Kreuzer und #{pfennigeIn2} Pfennige"
-      puts "Ergeben #{talerOut} Taler, #{kreuzerOut} Kreuzer und #{pfennigeOut} Pfennige!"
+      puts "Bitte geben Sie den zweiten Betrag ein, den Sie addieren möchten! (Taler, Kreuzer, Pfennige)"
+      Am2 = Amount.new(gets.to_i, gets.to_i, gets.to_i)
+      Am3 = Amount.new(0, 0, Am1.totPret + Am2.totPret)
+      #puts "#{talerIn1} Taler, #{kreuzerIn1} Kreuzer und #{pfennigeIn1} Pfennige #{op} #{talerIn2} Taler, #{kreuzerIn2} Kreuzer und #{pfennigeIn2} Pfennige"
+      Am3.totErg
     when "-"
       puts "Bitte geben Sie den zweiten Betrag ein, den Sie subtrahieren möchten!"
-      puts "Wie viele Taler?"
-      talerIn2 = gets.to_i
-      puts "Wie viele Kreuzer?"
-      kreuzerIn2 = gets.to_i
-      puts "Wie viele Pfennige?"
-      pfennigeIn2 = gets.to_i
-      pfennigeTot2 = (talerIn2*wkk*wkp) + (kreuzerIn2*wkp) + pfennigeIn2
-      ergebnis = pfennigeTot1.to_i - pfennigeTot2.to_i
-      talerOut = ergebnis / (wkk*wkp) .floor
-      kreuzerOut = (ergebnis - (talerOut*wkk*wkp)) / wkp .floor
-      pfennigeOut = (ergebnis - (talerOut*wkk*wkp) - (kreuzerOut*wkp))
-      puts "#{talerIn1} Taler, #{kreuzerIn1} Kreuzer und #{pfennigeIn1} Pfennige #{op} #{talerIn2} Taler, #{kreuzerIn2} Kreuzer und #{pfennigeIn2} Pfennige"
-      puts "Ergeben #{talerOut} Taler, #{kreuzerOut} Kreuzer und #{pfennigeOut} Pfennige!"
+      Am2 = Amount.new(gets.to_i, gets.to_i, gets.to_i)
+      Am3 = Amount.new(0, 0, Am1.totPret - Am2.totPret)
+      #puts "#{talerIn1} Taler, #{kreuzerIn1} Kreuzer und #{pfennigeIn1} Pfennige #{op} #{talerIn2} Taler, #{kreuzerIn2} Kreuzer und #{pfennigeIn2} Pfennige"
+      Am3.totErg
     when "/"
       puts "Durch welchen Faktor möchten Sie dividieren?"
       divisor = gets.to_i
-      ergebnis = pfennigeTot1 / divisor .round
-      talerOut = ergebnis / (wkk*wkp) .floor
-      kreuzerOut = (ergebnis - (talerOut*wkk*wkp)) / wkp .floor
-      pfennigeOut = (ergebnis - (talerOut*wkk*wkp) - (kreuzerOut*wkp))
-      puts "#{talerIn1} Taler, #{kreuzerIn1} Kreuzer und #{pfennigeIn1} Pfennige / #{divisor}"
-      puts "Ergeben #{talerOut} Taler, #{kreuzerOut} Kreuzer und #{pfennigeOut} Pfennige!"
+      Am3 = Amount.new(0, 0, Am1.totPret / divisor)
+      #puts "#{talerIn1} Taler, #{kreuzerIn1} Kreuzer und #{pfennigeIn1} Pfennige / #{divisor}"
+      Am3.totErg
     when "*"
       puts "Mit welchem Faktor möchten Sie multiplizieren?"
       multiplikator = gets.to_i
-      ergebnis = pfennigeTot1 * multiplikator
-      talerOut = ergebnis / (wkk*wkp) .floor
-      kreuzerOut = (ergebnis - (talerOut*wkk*wkp)) / wkp .floor
-      pfennigeOut = (ergebnis - (talerOut*wkk*wkp) - (kreuzerOut*wkp))
-      puts "#{talerIn1} Taler, #{kreuzerIn1} Kreuzer und #{pfennigeIn1} Pfennige * #{multiplikator}"
-      puts "Ergeben #{talerOut} Taler, #{kreuzerOut} Kreuzer und #{pfennigeOut} Pfennige!"
+      Am3 = Amount.new(0, 0, Am1.totPret * multiplikator)
+      #puts "#{talerIn1} Taler, #{kreuzerIn1} Kreuzer und #{pfennigeIn1} Pfennige * #{multiplikator}"
+      Am3.totErg
     when "="
-      ergebnis = pfennigeTot1
-      talerOut = ergebnis / (wkk*wkp) .floor
-      kreuzerOut = (ergebnis - (talerOut*wkk*wkp)) / wkp .floor
-      pfennigeOut = (ergebnis - (talerOut*wkk*wkp) - (kreuzerOut*wkp))
-      puts "#{talerIn1} Taler, #{kreuzerIn1} Kreuzer und #{pfennigeIn1} Pfennige sind korrekt notiert #{talerOut} Taler, #{kreuzerOut} Kreuzer und #{pfennigeOut} Pfennige!"
+      #puts "#{talerIn1} Taler, #{kreuzerIn1} Kreuzer und #{pfennigeIn1} Pfennige sind korrekt notiert #{talerOut} Taler, #{kreuzerOut} Kreuzer und #{pfennigeOut} Pfennige!"
+      Am1.totErg
     else
       puts "Fehlerhafte Eingabe! Bitte verwenden Sie nur ein eines der angeführten Zeichen zur Eingabe."
       puts "Bitte starten Sie das Script neu!"
       abort
     end
+=begin
     puts "Möchten Sie mit diesem Ergebnis weiterrechnen (y/n)?"
     decision = gets.chomp
     if decision == "y"
-      pfennigeTot1 = ergebnis
-      talerIn1 = talerOut
-      kreuzerIn1 = kreuzerOut
-      pfennigeIn1 = pfennigeOut
+      Am1 = Am3
     end
     break if decision != "y"
   end
@@ -98,7 +84,10 @@ loop do
   decision2 = gets.chomp
   break if decision2 != "y"
 end
+=end
 puts "Auf Wiedersehen!"
+
+
 
 #Der default verwendet Wechselkurs lautet: 1 Taler = 36 Kreuzer. 1 Kreuzer = 6 Pfennige.
 #Dieser Kurs entstammt einer Rechnung eines Hexenprozesses gegen Herman Buschorn 1654 aus dem Stadtarchiv Lemgo (v.num 10106)
